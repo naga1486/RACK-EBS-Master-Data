0ee685a65bc8501fb9b59e55b049afe78fdf392f
naga1486 <nagarjuna.apps646@gmail.com>
2015-11-03 12:53:03 +0530

    Cash Management

---
+++ Results/Run_11-3-2015_12-43-45 PM/QTP Results/TC1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\admin\\Documents\\GitHub\\RACK-EBS-Master-Data\\Results\\Run_11-3-2015_12-43-45 PM\\QTP Results\\TC1',namePairs:[]}]};
---
+++ Results/Run_11-3-2015_12-47-49 PM/QTP Results/TC1/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\admin\\Documents\\GitHub\\RACK-EBS-Master-Data\\Results\\Run_11-3-2015_12-47-49 PM\\QTP Results\\TC1',namePairs:[]}]};
