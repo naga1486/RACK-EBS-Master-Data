41dd2bb89808c9109041cbdda03ea5524aec040e
naga1486 <nagarjuna.apps646@gmail.com>
2015-11-03 17:26:57 +0530

    AR and Portal
    Modified AR Customer , invoice and receipt creation scripts and portal
    ACH payment script

---
+++ Results/Run_11-3-2015_5-14-35 PM/QTP Results/TC8/Report/test_data_src.js
@@ -0,0 +1,1 @@
+﻿var __testDataSource={items:[{dataFilePath:'Default.xls',dataFileFormat:'excel',dataFileFolder:'C:\\Users\\sravanthi\\Documents\\GitHub\\RACK-EBS-Master-Data\\Results\\Run_11-3-2015_5-14-35 PM\\QTP Results\\TC8',namePairs:[]}]};
